71d1d2ea8cfe899752c8fa0c73e2d8b9ed887f33
abijitabi47 <abijitib47thrissur@gmail.com>
2026-03-29 23:32:45 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
-class Student { 
-    public static void Test(String[] args) { 
+class Test { 
+    public static void main(String[] args) { 
         System.out.println("Student Name: Abijit I B"); 
         System.out.println("Register Number: 23MIC0013"); 
         System.out.println("Jenkins GitHub Integration Successful"); 

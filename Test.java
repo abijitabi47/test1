69c5de2c7cbb9440a5072606060438a9abed2275
abijitabi47 <abijitib47thrissur@gmail.com>
2026-03-29 23:19:48 +0530

    Add Student class with main method

---
+++ Test.java
@@ -0,0 +1,8 @@
+class Student { 
+    public static void main(String[] args) { 
+        System.out.println("Student Name: Abijit I B"); 
+        System.out.println("Register Number: 23MIC0013"); 
+        System.out.println("Jenkins GitHub Integration Successful"); 
+        System.out.println("Second Commit") 
+    } 
+} 
